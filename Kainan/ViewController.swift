68dc969423daa0d2ba4e60a5ec7dc23b689d22c1
Fulltime <fulltime@Fulltimes-iMac-136.local>
2017-10-15 14:12:41 +0800

    Initial Commit

---
+++ Kainan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kainan
+//
+//  Created by Fulltime on 15/10/2017.
+//  Copyright © 2017 University of the Philippines. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ KainanTests/KainanTests.swift
@@ -0,0 +1,36 @@
+//
+//  KainanTests.swift
+//  KainanTests
+//
+//  Created by Fulltime on 15/10/2017.
+//  Copyright © 2017 University of the Philippines. All rights reserved.
+//
+
+import XCTest
+@testable import Kainan
+
+class KainanTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
